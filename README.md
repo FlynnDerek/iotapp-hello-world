9ee0d74b37483c950049877a5e57b0d88341754d
Derek Flynn <flynnderek5@gmail.com>
2020-07-31 02:01:44 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# iotapp-hello-world-tutorial
+
+## Project setup
+```
+npm install
+```
+
+#### Add your iotapp app keys to the ``` getPrivateChain() ``` function in the ```mounted() ``` section of the ```HelloWorld.vue ``` file.
+```
+iotapp.getPrivateChain("yCeSGBSNAfnnpb6Nk0rkvis3GxcS5GDG", "sdtTy8Zt8MW3zsoohf8iDDwCTVpmyN3K")
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+ <table>
+        <thead>
+              <tr>
+                <th>Block Height</th>
+                <th>Block Hash</th>
+                <th>Previous Hash</th>
+                <th>Timestamp</th>
+                <th>Transactions</th>
+              </tr>
+         </thead>
+            <tbody>
+              <tr v-for="alias in chainData" id="tRow" :key="alias.index">
+                <td>{{alias.index}}</td>
+                <td>{{alias.hash}}</td>
+                <td>{{alias.prevHash}}</td>
+                <td>{{alias.timestamp}}</td>
+                <td>{{alias.transactions}}</td>
+              </tr>
+            </tbody>
+          </table>
+</template>
+
+<script>
+import iotapp from "iotapp-api" //Linked NPM module
+
+export default {
+data () {
+    return {
+      chainData: [],
+    }
+}, 
+methods: {
+
+},
+
+  mounted() {
+// Pass app key(s) as argument.
+iotapp.getPrivateChain("YOUR_IOTAPP_APP_KEY(S)")
+  .then(data => this.chainData = data)
+  .catch(err => console.log("Error occurred:", err))
+  },
+}
+</script>
